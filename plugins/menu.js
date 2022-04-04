db31c4daddf589807aa4c12cf0609c7a8aade329
Fahzz <101871868+Fahzz@users.noreply.github.com>
2022-04-04 17:33:49 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -232,8 +232,15 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
 ┃⬡ _*${conn.blocklist.length}*_ 𝙏𝙚𝙧𝙗𝙡𝙤𝙘𝙠
 ┃⬡ _*${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}*_ 𝘾𝙝𝙖𝙩 𝙩𝙚𝙧𝙗𝙖𝙣𝙣𝙚𝙙
 ┃⬡ _*${Object.entries(global.db.data.users).filter(user => user[1].banned).length}*_ 𝙋𝙚𝙣𝙜𝙜𝙪𝙣𝙖 𝙏𝙚𝙧𝙗𝙖𝙣𝙣𝙚𝙙
+┗━━━━━━━━⬣
+┏━━〔 𝐆𝐂𝐁𝐎𝐓 〕━⬣
+┃⬡ 𝙂𝙧𝙤𝙪𝙥 𝘽𝙤𝙩
+┃⬡ 
+┃⬡https://chat.whatsapp.com/KG1QdHJ3qkB5QlFW0KeNNJ
+┃⬡ 
 ┗━━━━━━━━⬣`.trim(),
-                        "footerText": "Create By Fahzz",
+
+                        "footerText": "*Create By Fahzz*",
                         "buttonText": "*Klik Disini*",
                         "listType": "SINGLE_SELECT",
                         "sections": [
