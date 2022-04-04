a2f27166971d50c52d56ea65581b727cf5e2d387
Fahzz <101871868+Fahzz@users.noreply.github.com>
2022-04-04 12:11:10 +0800

    Update viadana.js

--- plugins/viadana.js
+++ plugins/viadana.js
@@ -2,15 +2,15 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
 ╠═〘 PEMBAYARAN 〙 ═
 ╠═ Untuk Pembayaran Via Gopay
-╠➥ *6287892711054*
+╠➥ *6285754180729*
 ║- Owner Bot:
-║- wa.me/6287892711054
-╠═〘 Hyzer 〙 ═
+║- wa.me/6285754180729
+╠═〘 Fahzz 〙 ═
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 
 }
-handler.help = ['viadigi']
+handler.help = ['viagopay']
 handler.tags = ['info']
 handler.command = /^viadigi$/i
 
