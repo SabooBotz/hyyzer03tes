039a803e49514e2abd7f911a3404b5cbec1aea53
Fahzz <101871868+Fahzz@users.noreply.github.com>
2022-04-04 13:36:54 +0800

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,7 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
-Sc Nya Liat Di https://youtu.be/0wz53xj_5hI
-Jgn Lupa Di Subscribe yaa
+Sc :https://github.com/Fahzz/Hyzer-V3
 `.trim(), 'Fahzz', 'Siap Banh🗿', 'Thanks')
 handler.help = ['sourcecode']
 handler.tags = ['info']
