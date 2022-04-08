7cbbc6ebc2c9debae3bccbf3653583ca09cc8e00
SabooBotz <102595659+SabooBotz@users.noreply.github.com>
2022-04-09 00:02:22 +0800

    Update biodata.js

--- plugins/biodata.js
+++ plugins/biodata.js
@@ -20,7 +20,7 @@ let handler = async (m, { conn, text }) => {
 _Oke udah itu aja terimakasih_
 
 `.trim()
-conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'Raditya')).buffer(), ext, 'Recoded By Fahzz', 'Nomor Owner', '.owner', 'Donasi', '.donasi', 'Sewa Bot', '.sewa', m)
+conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'Raditya')).buffer(), ext, 'Recoded By Sabo', 'Nomor Owner', '.owner', 'Donasi', '.donasi', 'Sewa Bot', '.sewa', m)
 
 }
 handler.help = ['infoowner', 'infocreator']
