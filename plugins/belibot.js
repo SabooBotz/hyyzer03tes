74b3aed3b8e70d9455a21ebcc368f3707ff0d553
SabooBotz <102595659+SabooBotz@users.noreply.github.com>
2022-04-08 23:59:15 +0800

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
 	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Beli Bot 〙 ═
 ╠➥ *PERMINGGU* : *Rp.3.000*
-╠➥ *PERBULAN   : *Rp.5.000*
+╠➥ *PERBULAN*  : *Rp.5.000*
 ╠➥ *PERMANENT* : *Rp.10.000*
 ║
 ╠═〘 𝐏𝐄𝐌𝐁𝐀𝐘𝐀𝐑𝐀𝐍 〙 ═
@@ -11,7 +11,7 @@ let handler = async (m, { conn }) => {
 ╠═ Tertarik Untuk Beli Bot Ini?
 ╠➥Klil Button Di Bawah
 ║
-╠═〘 Sabo 〙 ═`.trim(), 'Sabo', 'Dana', '#viadana', 'Gopay', '#viagopay', m)
+╠═〘 Sabo 〙 ═`.trim(), 'Sabo', 'Menu', '#menu', 'Owner', '#owner', m)
 }
 
 handler.command = /^sewabot$/i
