34dcec0bd7e2e57e76a216fe460b4510dcabc163
SabooBotz <102595659+SabooBotz@users.noreply.github.com>
2022-04-08 21:46:29 +0800

    Update faketroli.js

--- plugins/faketroli.js
+++ plugins/faketroli.js
@@ -23,7 +23,7 @@ let handler = async(m, { conn, args, text, usedPrefix: _p }) => {
                             status: 1,
                             surface : 1,
                             message: tytyd,
-                            orderTitle: "Fahzz",
+                            orderTitle: "Sabo",
                             thumbnail: fs.readFileSync("./src/mike.jpg"),
                             sellerJid: '0@s.whatsapp.net'
                         }
